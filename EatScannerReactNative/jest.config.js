bb1fcecebc44c5fc56718c25d80e40d48f6834bc
YoussefCode <yelmourabit@outlook.com>
2026-08-11 22:48:38 +0200

    Delete EatScannerReactNative directory

--- EatScannerReactNative/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: "react-native",
-  transform: {
-    "^.+\\.(ts|tsx)$": ["ts-jest", { tsconfig: "tsconfig.json" }]
-  },
-  testMatch: ["**/__tests__/**/*.test.ts"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
-};
